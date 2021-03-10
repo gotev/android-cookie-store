75a258c37d103079d6e693665adb4855315fcd90
Alex Gotev <alex@gotev.net>
2021-03-11 00:43:06 +0100

    #12 set expires directive only when maxAge is != -1. By default, -1 is indicating the cookie will persist until browser shutdown.

--- cookie-store/src/main/java/net/gotev/cookiestore/CookieStoreExtensions.kt
+++ cookie-store/src/main/java/net/gotev/cookiestore/CookieStoreExtensions.kt
@@ -9,12 +9,18 @@ import java.util.Locale
 import java.util.TimeZone
 
 fun HttpCookie.toSetCookieString(): String {
-    val dateFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.UK).apply {
-        timeZone = TimeZone.getTimeZone("GMT")
+    val expires = if (maxAge != -1L) {
+        val dateFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.UK).apply {
+            timeZone = TimeZone.getTimeZone("GMT")
+        }
+
+        val calendar = Calendar.getInstance(Locale.UK).apply { set(Calendar.SECOND, maxAge.toInt()) }
+
+        "; expires=${dateFormat.format(calendar.time)}"
+    } else {
+        ""
     }
 
-    val calendar = Calendar.getInstance(Locale.UK).apply { set(Calendar.SECOND, maxAge.toInt()) }
-    val expires = "; expires=${dateFormat.format(calendar.time)}"
     val path = if (path != null) "; path=$path" else ""
     val domain = if (domain != null) "; domain=$domain" else ""
     val secure = if (secure) "; secure" else ""
