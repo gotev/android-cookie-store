2c9c67bc4dbe5d30ce3d849abdda058e63f70743
Alex Gotev <alex@gotev.net>
2021-03-14 18:14:39 +0100

    #14 synchronize methods to prevent ConcurrentModificationException (#15)
    * #14 synchronize methods to prevent ConcurrentModificationException
    
    * fix: use commit instead of apply to leverage function locking and avoid race conditions

--- cookie-store/src/main/java/net/gotev/cookiestore/SharedPreferencesCookieStore.kt
+++ cookie-store/src/main/java/net/gotev/cookiestore/SharedPreferencesCookieStore.kt
@@ -16,64 +16,62 @@ open class SharedPreferencesCookieStore(
     private val gson = Gson()
 
     init {
-        preferences.all.forEach { (key, value) ->
-            try {
-                val index = URI.create(key)
-                val listType = object : TypeToken<ArrayList<HttpCookie>>() {}.type
+        synchronized(SharedPreferencesCookieStore::class.java) {
+            preferences.all.forEach { (key, value) ->
+                try {
+                    val index = URI.create(key)
+                    val listType = object : TypeToken<ArrayList<HttpCookie>>() {}.type
 
-                val cookies: ArrayList<HttpCookie> = gson.fromJson(value.toString(), listType)
+                    val cookies: ArrayList<HttpCookie> = gson.fromJson(value.toString(), listType)
 
-                uriIndex[index] = cookies
-            } catch (exception: Throwable) {
-                Log.e(
-                    javaClass.simpleName,
-                    "Error while loading key = $key, value = $value from cookie store named $name",
-                    exception
-                )
+                    uriIndex[index] = cookies
+                } catch (exception: Throwable) {
+                    Log.e(
+                        javaClass.simpleName,
+                        "Error while loading key = $key, value = $value from cookie store named $name",
+                        exception
+                    )
+                }
             }
         }
     }
 
-    override fun removeAll(): Boolean {
+    override fun removeAll(): Boolean = synchronized(SharedPreferencesCookieStore::class.java) {
         super.removeAll()
-
-        synchronized(this) {
-            preferences.edit().clear().apply()
-        }
-        return true
+        preferences.edit().clear().commit()
+        true
     }
 
     override fun add(uri: URI?, cookie: HttpCookie?) {
-        super.add(uri, cookie)
+        synchronized(SharedPreferencesCookieStore::class.java) {
+            super.add(uri, cookie)
 
-        uri?.let {
-            val index = getEffectiveURI(uri)
-            val cookies = uriIndex[index]
+            uri?.let {
+                val index = getEffectiveURI(uri)
+                val cookies = uriIndex[index]
 
-            synchronized(this) {
-                preferences.edit().putString(index.toString(), gson.toJson(cookies)).apply()
+                preferences.edit().putString(index.toString(), gson.toJson(cookies)).commit()
             }
         }
     }
 
-    override fun remove(uri: URI?, cookie: HttpCookie?): Boolean {
-        val result = super.remove(uri, cookie)
+    override fun remove(uri: URI?, cookie: HttpCookie?): Boolean =
+        synchronized(SharedPreferencesCookieStore::class.java) {
+            val result = super.remove(uri, cookie)
 
-        uri?.let {
-            val index = getEffectiveURI(uri)
-            val cookies = uriIndex[index]
+            uri?.let {
+                val index = getEffectiveURI(uri)
+                val cookies = uriIndex[index]
 
-            synchronized(this) {
                 preferences.edit().apply {
                     if (cookies == null) {
                         remove(index.toString())
                     } else {
                         putString(index.toString(), gson.toJson(cookies))
                     }
-                }.apply()
+                }.commit()
             }
-        }
 
-        return result
-    }
+            result
+        }
 }
